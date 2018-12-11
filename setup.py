8efb1f3c32006d826b8875b66e3294f4f3b1ec94
Matthew Kelly <bwe@usott-bwe-260.local>
2018-12-11 11:04:23 -0500

    Bumped version, loosened requirements, and made python 3 compatible

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup
 
 setup(name='l1',
-      version='0.1',
+      version='0.2',
       description='L1',
       author='Bugra Akyildiz',
       author_email='vbugra@gmail.com',
       url='bugra.github.io',
       packages=['l1'],
-      install_requires=['pandas==0.16.0',
-                        'cvxopt==1.1.7',
-                        'statsmodels==0.5.0',
+      install_requires=['pandas>=0.16.0',
+                        'cvxopt>=1.1.7',
+                        'statsmodels>=0.5.0',
                         ]
 
      )
